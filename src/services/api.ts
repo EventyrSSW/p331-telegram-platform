0da1575d9bedee127d6fb85ac8ff795c92392329
Oleg <oleh.voitenko@gmail.com>
2025-12-19 01:08:58 +0200

    fix: add ngrok-skip-browser-warning header

--- src/services/api.ts
+++ src/services/api.ts
@@ -23,6 +23,7 @@ class ApiService {
         ...options,
         headers: {
           'Content-Type': 'application/json',
+          'ngrok-skip-browser-warning': 'true',
           ...options?.headers,
         },
       });
